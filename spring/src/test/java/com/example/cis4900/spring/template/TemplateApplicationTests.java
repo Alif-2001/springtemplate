13f9951125ca60ee8bb89c0dc805a190b527c237
Alif-2001 <alifmerchant86@gmail.com>
2023-09-11 16:47:00 -0400

    fixed docker image build error

--- spring/src/test/java/com/example/cis4900/spring/template/TemplateApplicationTests.java
+++ spring/src/test/java/com/example/cis4900/spring/template/TemplateApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.cis4900.spring.template;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TemplateApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
